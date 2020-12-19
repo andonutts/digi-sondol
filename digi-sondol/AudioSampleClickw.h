f064f8b2c3e08e8e06bf69ce9de97fc32f67c49d
Andy He <smerchy@gmail.com>
2020-12-19 12:47:47 -0600

    Add click audio samples

--- digi-sondol/AudioSampleClickw.h
+++ digi-sondol/AudioSampleClickw.h
@@ -1,3 +1,3 @@
 // Audio data converted from WAV file by wav2sketch
 
-extern const unsigned int AudioSampleClick[6657];
+extern const unsigned int AudioSampleClickw[33];
--- digi-sondol/digi-sondol.ino
+++ digi-sondol/digi-sondol.ino
@@ -1,13 +1,18 @@
 // Digital Sample-based Sondol
 
 // WAV files converted to code by wav2sketch
-#include "AudioSampleClick.h" // https://freesound.org/people/Druminfected/sounds/250551/
+#include "AudioSampleClickw.h" // https://freesound.org/people/Druminfected/sounds/250551/
+#include "AudioSampleClick_louderw.h"
+#include "AudioSampleClick_even_louderw.h"
 
 #include <Audio.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
+
+// when defined, sets constant volume and frequency
+//#define TEST_MODE
 
 AudioPlayMemory          playMem1;
 AudioOutputI2S           i2s1;
@@ -21,7 +26,7 @@ int volPin = A2;
 elapsedMillis audioTimer;
 elapsedMillis inputTimer;
 elapsedMillis debugTimer;
-int audioInterval;
+uint16_t audioInterval;
 
 float getVolume() {
   int volVal = analogRead(volPin);
@@ -39,23 +44,39 @@ void setup() {
   sgtl5000_1.enable();
   sgtl5000_1.volume(getVolume());
   audioInterval = getAudioInterval();
+
+#ifdef TEST_MODE
+  sgtl5000_1.volume(0.2);
+  audioInterval = 500;
+#endif
 }
 
 void loop() {
   if (audioTimer > audioInterval) {
-    playMem1.play(AudioSampleClick);
+    playMem1.play(AudioSampleClick_even_louderw);
     audioTimer = 0;
   }
-  if (inputTimer > 100) {
+  
+#ifndef TEST_MODE
+  if (inputTimer > 10) {
     sgtl5000_1.volume(getVolume());
     audioInterval = getAudioInterval();
     inputTimer = 0;
   }
+#endif
+
   if (debugTimer > 1000) {
-    Serial.println("input info");
+    Serial.println("------------");
+    Serial.print("Volume: ");
     Serial.println(getVolume());
+    Serial.print("Audio interval (ms): ");
     Serial.println(audioInterval);
     Serial.println();
+    Serial.println("Raw analog readings");
+    Serial.print("volPin = ");
+    Serial.println(analogRead(volPin));
+    Serial.print("freqPin = ");
+    Serial.println(analogRead(freqPin));
     debugTimer = 0;
   }
 }
